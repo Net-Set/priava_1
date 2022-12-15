a792840bf8fb7b7d6e42962cf50da1c9c85b6633
Net-Set <somilsomil25@gmail.com>
2022-12-15 16:55:04 +0530

    15-12-22

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2022, 8, 10, 8)}); // year, month, date, hour
+        });
+});		
+
